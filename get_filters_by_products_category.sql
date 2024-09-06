62359f26ca9746884ceea36e787317333f6756f5
RomanTuras <salden.com@gmail.com>
2024-09-06 15:48:07 +0300

    Add two scripts

--- get_filters_by_products_category.sql
+++ get_filters_by_products_category.sql
@@ -16,3 +16,17 @@ left join filter_fields_uk as ff on ff.id=pff.id_filter_fields
 left join filters_uk as f on f.id=ff.id_filters
 where pff.id_products IN(SELECT id_products FROM products_categories where id_categories=100)
 GROUP BY ff.id_filters;
+
+-- Delete filter field from product by filter ID
+SELECT * FROM filter_fields_uk as ff 
+left join products_filter_fields as pff ON ff.id=pff.id_filter_fields 
+where ff.id_filters=10915 and pff.id_products=75938;
+
+DELETE pff 
+FROM products_filter_fields as pff
+INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
+WHERE ff.id_filters IN(10025,10026,10028,10040,10089,10175,10301,10317,10561,10731,10803,10903,10915,11986,11987,12059,12061,12062,12090,12166,12186,12216,12236)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=100);
+
+-- id_filters
+-- 10025,10026,10028,10040,10089,10175,10301,10317,10561,10731,10803,10903,10915,11986,11987,12059,12061,12062,12090,12166,12186,12216,12236
--- trigger_for_gift_product.sql
+++ trigger_for_gift_product.sql
@@ -1,3 +1,6 @@
+INSERT INTO delivery_type_ru (id, title) VALUES (3, "Курьер Теплорадость");
+INSERT INTO delivery_type_uk (id, title) VALUES (3, "Курʼєр Теплорадість");
+
 -- Clearing and inserting "Gift" sticker to the product, when id_products_present
 -- is changed from 0 to any ID
 DELIMITER $$ 
@@ -38,3 +41,10 @@ IF new.id_products_present = 0 THEN
 DELETE FROM products_tags WHERE id_tags=7 AND id_products=old.id;
 END IF; END$$ 
 DELIMITER ;
+
+DROP TRIGGER IF EXISTS delete_product_ru_present_trigger;
+DROP TRIGGER IF EXISTS update_product_ru_present_trigger;
+DROP TRIGGER IF EXISTS delete_product_present_trigger;
+DROP TRIGGER IF EXISTS update_product_present_trigger;
+
+-- SQLSTATE[42000]: Syntax error or access violation: 1142 TRIGGER command denied to user 'new.teploradost.com.ua'@'%' for table `test.teploradost.com.ua`.`products_uk` (SQL: update `products_uk` set `weight` = 0, `length` = 0, `width` = 0, `height` = 0, `id_products` = , `products_uk`.`updated_at` = 2024-08-23 14:34:13 where `id` = 200)
