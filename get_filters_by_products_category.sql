31d25bfd0d2641c410e37624e167b4c5ffc03074
RomanTuras <salden.com@gmail.com>
2024-10-29 07:43:05 +0200

    Add changes to script

--- get_filters_by_products_category.sql
+++ get_filters_by_products_category.sql
@@ -14,7 +14,7 @@ where pff.id_products=75938;
 SELECT ff.id_filters, f.title FROM products_filter_fields as pff
 left join filter_fields_uk as ff on ff.id=pff.id_filter_fields
 left join filters_uk as f on f.id=ff.id_filters
-where pff.id_products IN(SELECT id_products FROM products_categories where id_categories=64)
+where pff.id_products IN(SELECT id_products FROM products_categories where id_categories=458)
 GROUP BY ff.id_filters;
 
 -- Delete filter field from product by filter ID
@@ -27,26 +27,59 @@ where ff.id_filters=10915 and pff.id_products=75938;
 DELETE pff 
 FROM products_filter_fields as pff
 INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
-WHERE ff.id_filters IN(10012,10040,10089,12055,12470)
-AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=462);
+WHERE ff.id_filters IN(10012,10020,10024,10026,10028,10288,10345,12061)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=458);
 
 DELETE pff 
 FROM products_filter_fields as pff
 INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
-WHERE ff.id_filters IN(10012,10019,10089,10200,10345,10622,10638,10895,10896,10942,12055,12470)
-AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=453);
+WHERE ff.id_filters IN(10012,10025,10026,10028,10089,10104,12468)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=513);
 
 DELETE pff 
 FROM products_filter_fields as pff
 INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
-WHERE ff.id_filters IN(10012,10016,10023,10024,10028,10033,10040,10089,10142,10197,10339,10895,11986,11987,12009,12051,12055,12061,12062,12463)
-AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=457);
+WHERE ff.id_filters IN(10012,10025,10026,10028,10089,10638,10942)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=463);
 
 DELETE pff 
 FROM products_filter_fields as pff
 INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
-WHERE ff.id_filters IN(10012,10016,10023,10024,10028,10033,10040,10142,10339,12463)
-AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=508);
+WHERE ff.id_filters IN(10025,10028,10895,12553)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=511);
 
+DELETE pff 
+FROM products_filter_fields as pff
+INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
+WHERE ff.id_filters IN(10012,10024,10028,12553)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=512);
+
+DELETE pff 
+FROM products_filter_fields as pff
+INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
+WHERE ff.id_filters IN(10012,10016,10021,10024,10025,10028,10032,10034,10051,10089,10895,10898,12461,12463,12553,12608)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=514);
+
+DELETE pff 
+FROM products_filter_fields as pff
+INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
+WHERE ff.id_filters IN(10012,10025,10895,12553)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=464);
+
+DELETE pff 
+FROM products_filter_fields as pff
+INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
+WHERE ff.id_filters IN(10012,12553)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=509);
+
+DELETE pff 
+FROM products_filter_fields as pff
+INNER JOIN filter_fields_uk as ff ON ff.id=pff.id_filter_fields 
+WHERE ff.id_filters IN(10012,10017,10028,10896,10942)
+AND pff.id_products IN(SELECT id_products FROM products_categories where id_categories=466);
 
 -- id_filters
+
+
+-- id categories
+458,513,463,511,512,514,464,509,466
